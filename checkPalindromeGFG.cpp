78ac7187c7a80b2d4c7078d0e40ba81aeb9863f4
Parth Gartan <parthgartan26feb@gmail.com>
2026-01-18 16:01:12 +0530

    Refactor palindrome check for cleaner implementation
    Removed unnecessary whitespace and comments for cleaner code.

--- checkPalindromeGFG.cpp
+++ checkPalindromeGFG.cpp
@@ -1,25 +1,17 @@
 class Solution {
   public:
-  
   bool recursionFunc ( string &s, int ptr1, int ptr2) {
-      
       if( ptr1 >= ptr2 ) return true;
-      
-     
         if ( s[ptr1] != s[ptr2] ){
           return false;
       }
-        
-      
       return recursionFunc ( s, ptr1+1, ptr2-1 );
   }
-  
     bool isPalindrome(string& s) {
         
         int n = s.size();
         int ptr1 = 0;
         int ptr2 = n-1;
-        
         return recursionFunc(s, ptr1, ptr2);
     }
 };
