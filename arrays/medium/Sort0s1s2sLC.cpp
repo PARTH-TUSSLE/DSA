f83d8a056509b52fca6d345e429286c4ab07e4e3
Parth Gartan <parthgartan26feb@gmail.com>
2026-01-29 22:22:18 +0530

    Implement sortColors function to sort 0s, 1s, and 2s

---
+++ arrays/medium/Sort0s1s2sLC.cpp
@@ -0,0 +1,32 @@
+// brute -> merge sort
+// better -> count no of 0s, 1s and 2s
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int zeros = 0;
+        int ones = 0;
+        int twos = 0;
+
+        for ( int i = 0; i < nums.size(); i++ ) {
+            if ( nums[i] == 0 ) {
+                zeros +=1;
+            } else if ( nums[i] == 1 ) {
+                ones +=1;
+            } else {
+                twos +=1;
+            }
+        }
+        for ( int i = 0; i < zeros; i++ ) {
+            nums[i] = 0;
+        }
+        for ( int i = zeros; i < zeros + ones; i++ ) {
+            nums[i] = 1;
+        }
+        for ( int i = zeros + ones; i < zeros + ones + twos; i++ ) {
+            nums[i] = 2;
+        }
+    }
+};
+
+//
