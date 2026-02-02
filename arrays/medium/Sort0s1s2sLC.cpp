0ae25e0a6424b93f01fdb53674ab9e0de1d16538
Parth Gartan <parthgartan26feb@gmail.com>
2026-02-02 20:06:24 +0530

    Implement Dutch-National Flag algorithm for sorting

--- arrays/medium/Sort0s1s2sLC.cpp
+++ arrays/medium/Sort0s1s2sLC.cpp
@@ -29,4 +29,25 @@ public:
     }
 };
 
-//
+// Dutch-National Flag algorithm
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int low = 0;
+        int mid = 0;
+        int n = nums.size();
+        int high = n - 1;
+        while ( mid <= high ) {
+            if ( nums[mid] == 0 ) {
+                swap(nums[low], nums[mid]);
+                low ++;
+                mid ++;
+            } else if ( nums[mid] == 1 ) {
+                mid ++;
+            } else {
+                swap(nums[mid], nums[high]);
+                high --;
+            }
+        }
+    }
+};
