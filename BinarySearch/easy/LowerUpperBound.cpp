b44c5b2cd294e4168656e374c7c9c46549e90a96
Parth Gartan <parthgartan26feb@gmail.com>
2026-02-05 23:00:40 +0530

    Add upperBound method to Solution class
    Implemented upper bound function for binary search.

---
+++ BinarySearch/easy/LowerUpperBound.cpp
@@ -0,0 +1,38 @@
+// lower bound
+class Solution {
+  public:
+    int lowerBound(vector<int>& arr, int target) {
+        int low = 0;
+        int high = arr.size() -1 ;
+        int ans = arr.size();
+        while ( low <= high ) {
+            int mid = (low+high)/2;
+            if ( arr[mid] >= target ) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+    }
+};
+
+// upper - bound
+
+class Solution {
+  public:
+    int upperBound(vector<int>& arr, int target) {
+        int low = 0;
+        int high = arr.size() -1 ;
+        int ans = arr.size();
+        while ( low <= high ) {
+            int mid = (low+high)/2;
+            if ( arr[mid] > target ) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+    }
+};
