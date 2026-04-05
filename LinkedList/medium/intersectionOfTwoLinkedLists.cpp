90dbe98657812cf94588f37a0dc9cbe27e6dac5a
Parth Gartan <parthgartan26feb@gmail.com>
2026-04-05 15:44:13 +0530

    Implement intersection detection for two linked lists
    Added multiple implementations for finding the intersection of two linked lists using different approaches.

--- LinkedList/medium/intersectionOfTwoLinkedLists.cpp
+++ LinkedList/medium/intersectionOfTwoLinkedLists.cpp
@@ -36,4 +36,110 @@ public:
     }
 };
 
-//optimal
+//brute-2 -> hashmap
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        map<ListNode*, int>mpp;
+        ListNode* temp = headA;
+        while ( temp ) {
+            mpp[temp] = 1;
+            temp = temp->next;
+        }
+        temp = headB;
+        while ( temp ) {
+            if ( mpp.find(temp) != mpp.end() ) {
+                return temp;
+            }
+            temp = temp->next;
+        }
+        return NULL;
+    }
+};
+
+//optimal 1
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        int l1 = 0;
+        int l2 = 0;
+        ListNode* temp = headA;
+        if ( headA == NULL || headB == NULL ) {
+            return NULL;
+        }
+        while ( temp ) {
+            l1++;
+            temp = temp->next;
+        }
+        temp = headB;
+        while ( temp ) {
+            l2++;
+            temp = temp->next;
+        }
+        ListNode* ptr1 = headA;
+        ListNode* ptr2 = headB;
+        int diff = max(l1,l2) - min(l1,l2);
+        if (l1 > l2) {
+            for (int i = 0; i < l1 - l2; i++) {
+                ptr1 = ptr1->next;
+            }
+        } else {
+            for (int i = 0; i < l2 - l1; i++) {
+            ptr2 = ptr2->next;
+            }
+        }
+        while ( ptr1 && ptr2 ) {
+            if ( ptr1 == ptr2 ) {
+                return ptr1;
+            }
+            ptr1 = ptr1->next;
+            ptr2 = ptr2->next;
+        }
+        return NULL;
+    }
+};
+// optimal 2
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        if ( headA == NULL || headB == NULL ) {
+            return NULL;
+        }
+        ListNode* temp1 = headA;
+        ListNode* temp2 = headB;
+        while ( temp1 != temp2 ) {
+            temp1 = temp1->next;
+            temp2 = temp2->next;
+            if ( temp1 == temp2 ) {
+                return temp1;
+            }
+            if ( temp1 == NULL ) temp1 = headB;
+            if ( temp2 == NULL ) temp2 = headA;
+        }
+        return temp1;
+    }
+};
