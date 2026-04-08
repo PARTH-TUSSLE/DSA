df3f031d5c9c4d6cee9e462ca10e48264cf0472a
Parth Gartan <parthgartan26feb@gmail.com>
2026-04-08 18:48:18 +0530

    Add removeDuplicates function for sorted DLL
    Implement a function to remove duplicates from a sorted doubly linked list.

---
+++ LinkedList/DLL/removeDuplicatesFromASortedDoubly linkedList.cpp
@@ -0,0 +1,40 @@
+/*
+struct Node
+{
+    int data;
+    Node * next;
+    Node * prev;
+    Node (int x)
+    {
+        data=x;
+        next=NULL;
+        prev=NULL;
+    }
+
+};
+*/
+
+class Solution {
+  public:
+
+    Node *removeDuplicates(struct Node *head) {
+        Node* temp = head;
+        if ( head == NULL || head->next == NULL ) {
+            return head;
+        }
+        while ( temp && temp->next ) {
+            Node* nextNode = temp->next;
+            while ( nextNode && nextNode->data == temp->data ) {
+                Node* duplicate = nextNode;
+                nextNode = nextNode->next;
+                delete duplicate;
+            }
+            temp->next = nextNode;
+            if (nextNode) {
+                nextNode->prev = temp;
+            }
+            temp = temp->next;
+        }
+        return head;
+    }
+};
