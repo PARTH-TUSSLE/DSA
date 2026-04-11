01e5a126fc2ca8edc135ea2b58bacdc161b1e29c
Parth Gartan <parthgartan26feb@gmail.com>
2026-04-11 12:32:24 +0530

    Implement copyRandomList for deep copy of linked list

--- LinkedList/hard/138CopyListWithRandomPointer.cpp
+++ LinkedList/hard/138CopyListWithRandomPointer.cpp
@@ -37,3 +37,58 @@ public:
         return mpp[head];
     }
 };
+
+// optimal
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        if ( head == NULL ) {
+            return head;
+        }
+        Node* temp = head;
+        Node* dNode = new Node(-1);
+        Node* curr = dNode;
+        while ( temp ) {
+            Node* copyNode = new Node(temp->val);
+            copyNode->next = temp->next;
+            temp->next = copyNode;
+            temp = temp->next->next;
+        }
+        temp = head;
+        while ( temp ) {
+            Node* copyNode = temp->next;
+            if (temp->random) {
+                copyNode->random = temp->random->next;
+            } else {
+                copyNode->random = NULL;
+            }
+            temp = temp->next->next;
+        }
+        temp = head;
+        while ( temp ) {
+            Node* copyNode = temp->next;
+            curr->next = copyNode;
+            temp->next = temp->next->next;
+            temp = temp->next;
+            curr = curr->next;
+        }
+        return dNode->next;
+    }
+};
