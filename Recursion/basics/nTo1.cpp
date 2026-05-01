b9e8109dfcc7eeea25cebb05b7bff04de8d4e108
Parth Gartan <parthgartan26feb@gmail.com>
2026-05-01 14:14:50 +0530

    Modify helper function for counting from 1 to n
    Refactor helper function to count from 1 to n instead of n to 1.

--- Recursion/basics/nTo1.cpp
+++ Recursion/basics/nTo1.cpp
@@ -11,3 +11,20 @@ int main() {
     helper(10);
     return 0;
 }
+
+//backtrack
+
+#include <iostream>
+using namespace std;
+
+void helper ( int cnt, int  n ) {
+    if ( cnt > n ) return;
+    helper(cnt+1, n);
+    cout << cnt << endl;
+}
+
+int main() {
+    
+    helper(1, 5);
+    return 0;
+}
