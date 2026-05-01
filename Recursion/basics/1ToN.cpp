11da075fc1691bd5e9bc283c3f522db19e87a3d9
Parth Gartan <parthgartan26feb@gmail.com>
2026-05-01 12:43:03 +0530

    Refactor helper function for printing 1 to N

--- Recursion/basics/1ToN.cpp
+++ Recursion/basics/1ToN.cpp
@@ -11,3 +11,20 @@ int main() {
     helper(1,5);
     return 0;
 }
+
+// backtrack
+
+#include <iostream>
+using namespace std;
+
+void helper ( int  n ) {
+    if ( n == 0 ) return;
+    helper(n-1);
+    cout << n << endl;
+}
+
+int main() {
+    
+    helper(5);
+    return 0;
+}
