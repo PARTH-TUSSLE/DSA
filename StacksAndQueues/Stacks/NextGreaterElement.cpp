0588813f397d2f89dae77dbced75a54e4cfbdc9c
Parth Gartan <parthgartan26feb@gmail.com>
2026-07-05 22:31:35 +0530

    Optimize nextGreaterElement with unordered map
    Refactored nextGreaterElement function to use unordered map for optimal performance.

--- StacksAndQueues/Stacks/NextGreaterElement.cpp
+++ StacksAndQueues/Stacks/NextGreaterElement.cpp
@@ -23,3 +23,60 @@ public:
         return ans;
     }
 };
+
+//better
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        vector<int>nge (nums2.size());
+        stack<int>st;
+        vector<int>ans;
+        for ( int i = nums2.size()-1; i >= 0; i-- ) {
+            while ( !st.empty() && st.top() <= nums2[i] ) {
+                st.pop();
+            }
+            if ( st.empty() ) {
+                nge[i] = -1;
+            } else {
+                nge[i] = st.top();
+            }
+            st.push(nums2[i]);
+        }
+        for ( int i = 0; i < nums1.size(); i++ ) {
+            auto it = find(nums2.begin(), nums2.end(), nums1[i]);
+            int idx = it - nums2.begin();
+            ans.push_back(nge[idx]);
+        }
+        return ans;
+    }
+};
+
+//optimal -> use unordered map to store the NGE
+
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        stack<int> st;
+        unordered_map<int, int> mp;
+
+        for (int i = nums2.size() - 1; i >= 0; i--) {
+            while (!st.empty() && st.top() <= nums2[i]) {
+                st.pop();
+            }
+
+            if (st.empty())
+                mp[nums2[i]] = -1;
+            else
+                mp[nums2[i]] = st.top();
+
+            st.push(nums2[i]);
+        }
+
+        vector<int> ans;
+        for (int num : nums1) {
+            ans.push_back(mp[num]);
+        }
+
+        return ans;
+    }
+};
