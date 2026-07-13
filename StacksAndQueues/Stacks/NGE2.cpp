5cd423eda7b5a641aa1226ead2b50b036c3ae85d
Parth Gartan <parthgartan26feb@gmail.com>
2026-07-13 19:36:44 +0530

    Add NGE2.cpp for next greater element solution

---
+++ StacksAndQueues/Stacks/NGE2.cpp
@@ -0,0 +1,19 @@
+// brute
+
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> v(n, -1);
+        for ( int i = 0; i <= n-1; i++ ) {
+            for ( int j = i+1; j <= i+n-1; j++ ) {
+                int idx = j%n;
+                if ( nums[idx] > nums[i] ) {
+                    v[i] = nums[idx];
+                    break;
+                }
+            }
+        }
+        return v;
+    }
+};
