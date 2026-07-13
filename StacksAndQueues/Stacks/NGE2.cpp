9f37d55b5ccbf0a1a5ac8c10c356882ea1789980
Parth Gartan <parthgartan26feb@gmail.com>
2026-07-13 21:37:01 +0530

    Implement next greater elements using stack

--- StacksAndQueues/Stacks/NGE2.cpp
+++ StacksAndQueues/Stacks/NGE2.cpp
@@ -17,3 +17,28 @@ public:
         return v;
     }
 };
+
+// optimal -> MS
+
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        stack<int>st;
+        int n = nums.size();
+        vector<int> v(n);
+        for ( int i = (2*n)-1; i >= 0; i-- ) {
+            while ( !st.empty() && st.top() <= nums[i%n] ) {
+                st.pop();
+            }
+            if ( i < n ) {
+                if ( st.empty() ) {
+                    v[i] = -1;
+                } else {
+                v[i] = st.top();
+                }
+            }
+            st.push(nums[i%n]);
+        }
+        return v;
+    }
+};
